dc55b8ce111a4eddd7b328370aa487aa830dd2c5
lisovskyi-arsenii <lisovskyiarsenii@gmail.com>
2026-08-14 18:50:00 +0300

    fix: small fix in iteration of cookies so it does not throw NPE

--- security-starter-autoconfigure/src/main/java/com/lisovskyi/security/autoconfigure/cookie/CookieService.java
+++ security-starter-autoconfigure/src/main/java/com/lisovskyi/security/autoconfigure/cookie/CookieService.java
@@ -98,6 +98,10 @@ public class CookieService {
 
     private Optional<String> getCookieValue(HttpServletRequest request, String name) {
         Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return Optional.empty();
+        }
+
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals(name)) {
                 return Optional.of(cookie.getValue());
